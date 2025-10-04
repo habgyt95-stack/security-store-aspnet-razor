683e4ab8a5840d4e616172f831934db87eba2a8c
Ebrahim <Ebrahim@DESKTOP-J38U8MS>
2025-10-04 17:28:16 +0330

    commit

--- SecurityStore.Infrastructure/Migrations/20251004123814_AutoCreate.Designer.cs
+++ SecurityStore.Infrastructure/Migrations/20251004123814_AutoCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SecurityStore.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using SecurityStore.Infrastructure.Data;
 namespace SecurityStore.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251004123814_AutoCreate")]
+    partial class AutoCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SecurityStore.Infrastructure/Migrations/20251004123814_AutoCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecurityStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutoCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
